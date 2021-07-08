545d9a27ef21f10138b99067407c1ee6c5f8880c
xz <xzreal_dlut@163.com>
2021-07-08 18:04:26 +0800

    0708

---
+++ mybatis-01/src/main/java/com/kuang/pojo/User.java
@@ -0,0 +1,13 @@
+package com.kuang.pojo;
+
+public class User {
+
+    private int id;  //id
+    private String name;   //姓名
+    private String pwd;   //密码
+
+    //构造,有参,无参
+    //set/get
+    //toString()
+
+}
